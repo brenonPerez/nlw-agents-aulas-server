e3cff6c654cc9b88400edc6e7a7d1cd90ebe2fc8
Brenon Tavares <brenonperez1997@gmail.com>
2025-07-12 23:00:18 -0300

    feat: gemini api integration and vector search

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -4,20 +4,22 @@ import { schema } from './schema/index.ts'
 
 await reset(db, schema)
 
-await seed(db, schema).refine((f) => {
-  return {
-    rooms: {
-      count: 20,
-      columns: {
-        name: f.companyName(),
-        description: f.loremIpsum(),
+await seed(db, { rooms: schema.rooms, questions: schema.questions }).refine(
+  (f) => {
+    return {
+      rooms: {
+        count: 5,
+        columns: {
+          name: f.companyName(),
+          description: f.loremIpsum(),
+        },
       },
-    },
-    questions: {
-      count: 20,
-    },
+      questions: {
+        count: 20,
+      },
+    }
   }
-})
+)
 
 await sql.end()
 
